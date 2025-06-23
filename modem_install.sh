1a729f09f14bfc34295f4cd607193e79dd118e17
nik632 <nik.peltier@gmail.com>
2025-06-23 13:08:14 -0500

    Update modem_install.sh to use home directory for navigation and cleanup

--- modem_install.sh
+++ modem_install.sh
@@ -2,11 +2,11 @@
 #curl -s -L https://github.com/peltierco-com/fax-host/raw/refs/heads/main/modem_install.sh | bash
 
 #git clone
-cd /home/admin
-rm -rf /home/admin/fax-modem && /
+cd ~
+rm -rf ~/fax-modem && /
 git clone https://github.com/peltierco-com/fax-modem.git
 
 #docker build
 sudo usermod -aG docker $USER && newgrp docker
-cd /home/admin/fax-modem/docker/ubuntu
+cd ~/fax-modem/docker/ubuntu
 docker build -t fax-modem:latest .
