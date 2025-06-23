321fe195eba9ddd4f72c4d23819fbe81a27e85b1
nik632 <nik.peltier@gmail.com>
2025-06-23 12:49:23 -0500

    Add modem_install.sh script to clone fax-modem repository

---
+++ modem_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#curl -s -L https://github.com/peltierco-com/fax-host/raw/refs/heads/main/modem_install.sh | bash
+
+#git modem
+cd ~
+git clone https://github.com/peltierco-com/fax-modem.git
+cd fax-modem
